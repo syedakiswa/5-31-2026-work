63de0a6aae3f44b0d3b13c91035afd083ccf998b
shifaaamir89-dotcom <shifaaamir89@gmail.com>
2026-05-31 11:57:05 +0500

    Inital commit

---
+++ mian.js
@@ -0,0 +1,25 @@
+// arrow function
+// modern js or react js
+// past
+
+function greet(){
+    console.log("Hello Hina")
+}
+
+greet()
+
+// why we need arrow function ??
+
+function sum(a,b){
+  return a+b
+console.log(sum)
+}
+sum(2+3)
+
+
+let add= (a,b) => {console.log(a+b)}
+add(2,4)
+
+
+let singlePr = name => {console.log(name)}
+singlePr();
